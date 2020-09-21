c8c7edef9a466e21eb0e15578674722b692aba37
HelgaLuksevica <helga.luksevica@gmail.com>
2020-09-21 20:16:06 +0300

    exercise8

--- src/game/exercise8.java
+++ src/game/exercise8.java
@@ -12,10 +12,53 @@ public class exercise8 {
                     {' '}, {'|'}, {' '}, {'|'}, {' '},
                     {' '}, {'|'}, {' '}, {'|'}, {' '}};
 
+        printTheGameBoard(board);
+
         Scanner scan = new Scanner (System.in);
-        System.out.println("Enter your place (1-9): ");
+        System.out.print("Enter your place (1-9): ");
         int place = scan.nextInt();
 
         System.out.println(place);
+
+           switch(place)
+
+    {
+        case 1:
+            board[0][0] = 'X';
+            break;
+        case 2:
+        board[0][2] = 'X';
+        break;
+        case 3:
+        board[0][4] = 'X';
+        break;
+        case 4:
+        board[2][0] ='X';
+        break;
+        case 5:
+        board[2][2] ='X';
+        break;
+        case 6:
+        board[2][4] ='X';
+        break;
+        case 7:
+        board[4][0] ='X';
+        break;
+        case 8:
+        board[4][2] ='X';
+        break;
+        case 9:
+        board[4][4] ='X';
+        break;
+    }
+    printTheGameBoard(board);
+}
+    public static void printTheGameBoard(char[][] board) {
+        for (char[]row : board) {
+            for(char symbol :row){
+                System.out.println(symbol);
+            }
         }
+
+    }
 }
