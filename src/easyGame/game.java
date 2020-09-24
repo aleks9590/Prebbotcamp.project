48ab7f910f0fa0b0ed3236642591c909e8127a47
aleks9590 <70863853+aleks9590@users.noreply.github.com>
2020-09-24 18:49:53 +0300

    new code 1

---
+++ src/easyGame/game.java
@@ -0,0 +1,109 @@
+package easyGame;
+
+public class game {
+        private char[][] board;
+        private char player;
+
+        public game() {
+            board = new char[3][3];
+            player = 'X';
+            createBoard();
+        }
+
+        public char getPlayer(){
+            return player;
+        }
+
+        public void createBoard() {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    board[i][j] = ' ';
+                }
+            }
+        }
+
+        public void printBoard() {
+            System.out.println("~~~~~~~~~~~~~");
+            for (int i = 0; i < 3; i++) {
+                System.out.print("| ");
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(board[i][j] + " | ");
+                }
+                System.out.println();
+                System.out.println("~~~~~~~~~~~~~");
+            }
+        }
+
+        public boolean thereIsNoSpace(){
+            boolean thereIsNoSpace = true;
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] == ' ') {
+                        thereIsNoSpace = false;
+                    }
+                }
+            }
+            return thereIsNoSpace;
+        }
+        boolean lookForWinner(){
+            return (lookRowForWinner() || lookColForWinner() || lookDiagonalForWinner());
+        }
+
+        private boolean lookRowForWinner() {
+            for (int i = 0; i < 3; i++) {
+                if (determineSimilarity(board[i][0], board[i][1], board[i][2]) == true) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private boolean lookColForWinner(){
+            for (int i = 0; i < 3; i++) {
+                if (determineSimilarity(board[0][i], board[1][i], board[2][i]) == true) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private boolean lookDiagonalForWinner(){
+            for (int i = 0; i < 3; i++) {
+                if ((determineSimilarity(board[0][0], board[1][1], board[2][2]) == true)
+                        || (determineSimilarity(board[0][2], board[1][1], board[2][0]) == true)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private boolean determineSimilarity(char col1, char col2, char col3){
+            return ((col1 == col2) &&
+                    (col2 == col3) &&
+                    (col1 != ' '));
+        }
+
+        public void whoseTurn() {
+            if (player == 'X') {
+                player = 'O';
+            }
+            else {
+                player = 'X';
+            }
+        }
+
+        public boolean placePlayer(int row, int col) {
+
+            if ((row >= 0) && (row < 3)) {
+                if ((col >= 0) && (col < 3)) {
+                    if (board[row][col] == ' ') {
+                        board[row][col] = player;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
---
+++ src/easyGame/gameOutput.java
@@ -0,0 +1,35 @@
+package easyGame;
+import java.util.Scanner;
+
+public class gameOutput {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        game ticTacToe = new game();
+        ticTacToe.createBoard();
+        System.out.println("Welcome to Tic-tac-toe game!");
+
+        do {
+            System.out.println("Here is our board: ");
+            ticTacToe.printBoard();
+            int row;
+            int col;
+
+            do {
+                System.out.println("Player" + ticTacToe.getPlayer() + ", enter Your numbers please! ");
+                row = scan.nextInt() - 1;
+                col = scan.nextInt() - 1;
+            }
+            while (!ticTacToe.placePlayer(row, col));
+            ticTacToe.whoseTurn();
+        }
+        while (!ticTacToe.lookForWinner() && !ticTacToe.thereIsNoSpace());
+        if (ticTacToe.thereIsNoSpace() && !ticTacToe.lookForWinner()) {
+            System.out.println("It's a tie!");
+        } else {
+            System.out.println("Here is our board: ");
+            ticTacToe.printBoard();
+            ticTacToe.whoseTurn();
+            System.out.println("Congratulations, " + Character.toUpperCase(ticTacToe.getPlayer()) + " You win!");
+        }
+    }
+}
